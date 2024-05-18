93ab5d2f5152987a4785d701841d93da3a888b04
seunghyeonKang <number42isuser@gmail.com>
2024-05-19 08:56:27 +0900

    [0519] 에브리타임

--- everytime/js/login.js
+++ everytime/js/login.js
@@ -1,1 +1,25 @@
-let form_id = document.getElementById("form_id");
+const formIdElement = document.getElementById("form_id");
+const formPasswordElement = document.getElementById("form_password");
+const formSubmitButton = document.getElementById("form_submit");
+
+function validation() {
+  const formIdValue = formIdElement.value.trim();
+  const formPasswordValue = formPasswordElement.value.trim();
+
+  if (formIdValue !== "" && formPasswordValue !== "") {
+    on();
+  } else {
+    off();
+  }
+}
+
+function off() {
+  formSubmitButton.classList.remove("on");
+}
+
+function on() {
+  formSubmitButton.classList.add("on");
+}
+
+formIdElement.addEventListener("input", validation);
+formPasswordElement.addEventListener("input", validation);
