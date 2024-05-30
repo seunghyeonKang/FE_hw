238c67d5f553c3ffb3bdcb237c0348e08e19f750
seunghyeonKang <number42isuser@gmail.com>
2024-05-30 13:54:02 +0900

    에타 js

--- everytime/js/detail.js
+++ everytime/js/detail.js
@@ -1,8 +1,10 @@
 // 좋아요, 스크랩
 const articleLikesCount = document.querySelector("#article_likes_count");
 const articleScrabsCount = document.querySelector("#article_scrabs_count");
-const articleLikesText = document.querySelector("#like_text");
 const articleScrabsText = document.querySelector("#scrap_text");
+const articleScrabsIcon = document.querySelector(
+  "#article_scrab_button .imgBox_icons"
+);
 
 let isLiked = false;
 let isScrabed = false;
@@ -15,22 +17,20 @@ document
   .addEventListener("click", articleScrabClicked);
 
 function articleLikeClicked() {
-  if (isLiked) {
-    articleLikesCount.innerText--;
-    articleLikesText.innerText = "공감";
-  } else {
+  if (!isLiked) {
     articleLikesCount.innerText++;
-    articleLikesText.innerText = "공감 취소";
+    isLiked = !isLiked;
   }
-  isLiked = !isLiked;
 }
 function articleScrabClicked() {
   if (isScrabed) {
     articleScrabsCount.innerText--;
     articleScrabsText.innerText = "스크랩";
+    articleScrabsIcon.style.display = "block";
   } else {
     articleScrabsCount.innerText++;
     articleScrabsText.innerText = "스크랩 취소";
+    articleScrabsIcon.style.display = "none";
   }
   isScrabed = !isScrabed;
 }
